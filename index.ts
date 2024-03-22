dfc2fdd71abbbddc23e9d7cb3e423748751f3203
JacyntheErdman <benjaminlaban6dd3e46801725@outlook.com>
2024-03-23 06:07:23 +0800

    repudiandae

---
+++ index.ts
@@ -0,0 +1,16 @@
+import {configureRest} from './lib/rest-config';
+import * as routerConfig from './lib/router-config';
+import {BasicRoutes} from './lib/router';
+import {serverConfig} from './lib/server-config';
+import {AppRequest, AppResponse} from './lib/types';
+import {handlerDecorator} from './lib/utils';
+
+export {
+  AppRequest,
+  AppResponse,
+  BasicRoutes,
+  configureRest,
+  routerConfig,
+  serverConfig,
+  handlerDecorator
+};
